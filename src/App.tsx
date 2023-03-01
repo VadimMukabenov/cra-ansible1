bb902d398654c3ab48147785a32bf0e680bed1ff
Vadim <vadim.mukabenov98@gmail.com>
2023-03-02 00:12:44 +0300

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          { count }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => setCount((prev: number) => prev + 1)}>Click</button>
     </div>
   );
 }
